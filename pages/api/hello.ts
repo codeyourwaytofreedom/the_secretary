2a9fe2f7905691a41e14404d0d71362b4777c3d9
codeyourwaytofreedom <can.kolej@gmail.com>
2023-04-07 17:06:29 +0300

    alternative connection in place.
    from node/mongodb documentary.
    
    Keep going.
    
    Remember why you started.

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { connectToDatabase } from './mongodb';
+//import { connectToDatabase } from './mongodb';
+import { client } from "./mongodb"; 
 
 type Data = {
   name: string
@@ -10,10 +11,9 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const client = await connectToDatabase();
   
   const databasesList = await client.db().admin().listDatabases();
   console.log("Databases:", databasesList.databases.map(db => db.name));
 
-  res.status(200).json({ name: databasesList.databases.length.toString()+" the number of databases" });
+  res.status(200).json({ name: databasesList.databases.length.toString()+" adet veri tabanı var" });
 }
