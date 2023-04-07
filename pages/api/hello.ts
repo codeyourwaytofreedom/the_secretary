4aa2a177dc580eb721f1b58d3568e92bfbb60863
codeyourwaytofreedom <can.kolej@gmail.com>
2023-04-07 18:15:26 +0300

    First stunt done.
    Connecting to the db.
    Retrieving items.
    Publishing them to the endpoint.
    
    Great step forward.
    
    Keep going.
    
    Remember why you started.

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,19 +1,28 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-//import { connectToDatabase } from './mongodb';
-import { client } from "./mongodb"; 
+import { connectToDatabase } from './mongodb';
 
 type Data = {
-  name: string
+  name: object[]
 }
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  
+  const client = await connectToDatabase();
   const databasesList = await client.db().admin().listDatabases();
-  console.log("Databases:", databasesList.databases.map(db => db.name));
 
-  res.status(200).json({ name: databasesList.databases.length.toString()+" adet veri tabanı var" });
+  const databaseName = 'sample_guides';
+  const collectionName = 'planets';
+
+  const db = client.db(databaseName);
+  const collection = db.collection(collectionName);
+  const documents = await collection.find().toArray();
+
+  console.log(documents)
+
+
+  console.log("Databases:", databasesList.databases.map(db => db.name));
+  res.status(200).json( documents );
 }
--- pages/api/mongodb.js
+++ pages/api/mongodb.js
@@ -1,40 +1,7 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // Replace the placeholder with your Atlas connection string
-const uri = process.env.MONGODB_URI;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-export const client = new MongoClient(uri,  {
-        serverApi: {
-            version: ServerApiVersion.v1,
-            strict: true,
-            deprecationErrors: true,
-        }
-    }
-);
-
-async function run() {
-  try {
-    // Connect the client to the server (optional starting in v4.7)
-    await client.connect();
-
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
-
-
-
-
-
-//alternative connection method
-/* export async function connectToDatabase() {
+export async function connectToDatabase() {
   const client = await MongoClient.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
   return client;
-} */
+}
