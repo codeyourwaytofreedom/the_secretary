01f4518025f2982e890c28557729bb1b74123f63
codeyourwaytofreedom <can.kolej@gmail.com>
2023-04-08 18:34:00 +0300

    Middleware working.
    Receives jwt and verifies it.
    
    Great progress today.
    
    Keep going.
    
    Remember why you started.

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,33 @@
 import { NextApiResponse } from "next";
-
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import { verify } from "jsonwebtoken";
+import * as jose from 'jose';
 
 
-interface ResponseWithCookies extends NextApiResponse {
-  cookies: any;
-}
-
-export function middleware(request: NextRequest) {
-  const response = NextResponse.next();
-
-  response.cookies.set("xxx","yyy")
-
+export async function middleware(request: NextRequest,response:NextResponse) {
+   response = NextResponse.next();
+  const allCookies = request.cookies.getAll();
+  const tokenCookie = allCookies.find(cookie => cookie.name === 'token');
+  if (tokenCookie) {
+/*     const jwt = tokenCookie.value;
+    console.log(jwt) */
+    const secret = new TextEncoder().encode(
+      'cc7e0d44fd473002f1c42167459001140ec6389b7353f8088f4d9a95f2f596f2',
+    )
+    const jwt =
+      'eyJhbGciOiJIUzI1NiJ9.eyJ1cm46ZXhhbXBsZTpjbGFpbSI6dHJ1ZSwiaWF0IjoxNjY5MDU2MjMxLCJpc3MiOiJ1cm46ZXhhbXBsZTppc3N1ZXIiLCJhdWQiOiJ1cm46ZXhhbXBsZTphdWRpZW5jZSJ9.C4iSlLfAUMBq--wnC6VqD9gEOhwpRZpoRarE0m7KEnI'
+    
+    const { payload, protectedHeader } = await jose.jwtVerify(jwt, secret)
+    
+    console.log(protectedHeader)
+    console.log(payload)
+    console.log("found")
+    response.headers.set('Authorization', `Bearer ${tokenCookie}`);
+  }
+  else{
+    console.log("not found")
+  }
   return response;
 }
 
-
-export const config = {
-  matcher: '/',
-}
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -3,10 +3,6 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { connectToDatabase } from './mongodb';
 import jwt from 'jsonwebtoken';
 
-type Data = {
-  name: object[]
-}
-
 type Patient = {
   name:string,
   surname:string,
@@ -14,10 +10,29 @@ type Patient = {
   gender:boolean
 }
 
+interface ResponseWithCookies extends NextApiResponse {
+  cookies: any;
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+
+  const token = jwt.sign({ userId: "7fgh" }, "skjvwrwr834745", {
+    expiresIn: '1h',
+  });
+  
+  res.setHeader('Set-Cookie', `token=${token}; HttpOnly; Max-Age=${60 * 60}; SameSite=Strict`);
+  // Send the response
+  res.status(200).json({ message: 'Cookie set successfully' });
+
+}
+
+
+
+
+//DATABASE CONNECTION
 /*   const client = await connectToDatabase();
 
   const col = client.db('clinic_06').collection('patients');
@@ -36,12 +51,3 @@ export default async function handler(
   
   res.status(200).json( documents );
   client.close(); */
-
-  const token = jwt.sign({ userId: "7fgh" }, "skjvwrwr834745", {
-    expiresIn: '1h',
-  });
-  console.log(token)
-  console.log(req.body)
-  res.setHeader('Set-Cookie', `token=${token}; HttpOnly`);
-  res.status(200).json( {res:"hiiiii"} );
-}
