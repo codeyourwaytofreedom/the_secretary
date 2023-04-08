f8fd7728c012567ed7dbc90c0ee944503ff35a21
codeyourwaytofreedom <can.kolej@gmail.com>
2023-04-09 00:50:19 +0300

    Route protected.
    Smooth process in NextJs.
    
    .env var added for JWT verify secret.
    
    Keep going...
    
    Remember why you started.

--- middleware.ts
+++ middleware.ts
@@ -9,13 +9,14 @@ export async function middleware(request: NextRequest) {
   const user_token = request.cookies.get('token')
   const jwt = user_token?.value;
   const secret = new TextEncoder().encode(
-    'skjvwrwr834745',
+    process.env.JWT_SECRET,
   )
   try {
     await jose.jwtVerify((jwt as string), secret);
     console.log("jwt valid")
   } catch (error) {
     console.log(error)
+    return NextResponse.redirect(new URL("/", request.url));
   }
 
   const response = NextResponse.next()
