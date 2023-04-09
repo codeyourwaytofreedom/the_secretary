2bf917f0e6682cdc761c49bd9ee5d54d767961ff
codeyourwaytofreedom <can.kolej@gmail.com>
2023-04-09 23:05:23 +0300

    Canvas in place.
    svg loader in place.
    
    Color issue solved.
    
    Types introduced.
    
    Great progress today.
    
    Keep going.
    
    Remember why you started.

--- components/padlock.tsx
+++ components/padlock.tsx
@@ -1,10 +1,10 @@
 import { Canvas, useThree } from "@react-three/fiber";
 import l from "../styles/Login.module.css";
-import { Color } from "three";
-import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import { SVGLoader } from 'three/examples/jsm/loaders/SVGLoader';
+import { useLoader } from '@react-three/fiber';
+import { Color,Shape } from "three";
 
 const CameraController = () => {
     const { camera, gl } = useThree();
@@ -22,14 +22,30 @@ const CameraController = () => {
     return null;
   };
 
+
+type svg_shape = {
+    shape:Shape;
+    color:Color;
+}
+
 const Padlock = () => {
+  const[lock,setLock] = useState<svg_shape[]>([]);
+  useEffect(() => {
+    const loader = new SVGLoader();
+    loader.load("/padlock.svg", function(data){
+        const padlock = data.paths.map((shp) => ({shape:SVGLoader.createShapes(shp)[0],color:shp.color }));
+        setLock(padlock);
+    });
+}, []);
+
+
     return (
     <div className={l.login_shell_padlock}>
         <Canvas>
         <CameraController/>
             <mesh>
-                <boxGeometry args={[1,1,1]} />
-                <meshStandardMaterial color={"red"}/>
+              <boxGeometry args={[2,2,2]} />
+              <meshBasicMaterial color={"navy"}/>
             </mesh>
         </Canvas>
     </div>
