f488cc51ab5b243795280c6c21c07dd1b4dffe0f
codeyourwaytofreedom <can.kolej@gmail.com>
2023-04-14 23:19:25 +0300

    Date update in place.
    Detail section in place.
    
    State as date... included in props.
    
    Keep going.
    
    Remember why you started.

--- components/calendar.tsx
+++ components/calendar.tsx
@@ -1,8 +1,22 @@
+import { NextPage } from 'next';
+import { useState } from 'react';
 import Calendar from 'react-calendar';
 
-const Cal = () => {
+interface date {
+    date:Date | undefined;
+    setDate: React.Dispatch<React.SetStateAction<Date | undefined>>;
+}
+
+const Cal:NextPage<date> = ({date, setDate}) => {
     return ( 
-            <Calendar locale={"tr-TR"} maxDate={new Date(2024, 0, 1)} minDate={new Date(2023, 0, 1)}/>         
+        <>
+        
+            <Calendar locale={"en-US"} 
+                maxDate={new Date(2024, 0, 1)} 
+                minDate={new Date(2023, 0, 1)}
+                onClickDay={(date) => {console.log(setDate(date)); console.log(typeof(date.toLocaleDateString("tr-TR")))}}
+            />     
+        </>    
      );
 }
  
--- pages/test.tsx
+++ pages/test.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import Cal from "../components/calendar";
 
 const Test = () => {
+    const [date, setDate] = useState<Date>();
     const today = new Date();
     const formattedDate = today.toLocaleDateString("tr-TR", {
         day: "2-digit",
@@ -16,7 +17,7 @@ const Test = () => {
         hour12: false,
       }));
 
-/*     useEffect(() => {
+    useEffect(() => {
     const interval = setInterval(() => {
         const time = new Date().toLocaleTimeString("tr-TR", {
             hour: "2-digit",
@@ -30,47 +31,59 @@ const Test = () => {
         return () => {
         clearInterval(interval);
         };
-    }, []); */
+    }, []);
 
 
     return ( 
     <>
-    <div className={a.console}>
-        <div className={a.console_current}>
-            <div className={a.console_current_top}>
+    <div className={a.double}>
+        <div className={a.console}>
+            <div className={a.console_current}>
+                <div className={a.console_current_top}>
+                    {
+                        [...Array(10)].map((e,i) =>
+                        <>
+                        <span key={i} id={a.holder}>
+                        </span>
+                        </>
+
+                    )
+                    }
+                </div>
+                <div className={a.console_current_center}>
+                        <div>
+                            <div>{formattedDate!}</div>
+                            <div suppressHydrationWarning>{currentTime!}</div>
+                        </div>
+                </div>
+                <div className={a.console_current_holes}>
                 {
-                    [...Array(10)].map((e,i) =>
-                    <>
-                    <span key={i} id={a.holder}>
-                    </span>
-                    </>
+                        [...Array(10)].map((e,i) =>
+                        <>
+                        <span key={i} id={a.oval}>
+                        </span>
+                        </>
 
-                )
-                }
+                    )
+                    }
+                </div>
             </div>
-            <div className={a.console_current_center}>
-                    <div>
-                        <div>{formattedDate!}</div>
-                        <div suppressHydrationWarning>{currentTime!}</div>
-                    </div>
+            <div className={a.console_calendar}>
+                <Cal date={date} setDate={setDate}/>
             </div>
-            <div className={a.console_current_holes}>
-            {
-                    [...Array(10)].map((e,i) =>
-                    <>
-                    <span key={i} id={a.oval}>
-                    </span>
-                    </>
-
-                )
-                }
+            <div>
+                <button className={a.console_edit}>LOG OUT</button>
             </div>
         </div>
-        <div className={a.console_calendar}>
-            <Cal/>
+        <div className={a.detail}>
+        <h1>{date && date!.toLocaleDateString("tr-TR", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric"
+      })}</h1>
         </div>
-        <div></div>
     </div>
+
         
     </>
         
